a908a2c254e29165d032ff9e899c5d71010fd115
480147530332 <145145478+480147530332@users.noreply.github.com>
2024-02-06 13:33:34 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const passwordbox =document.getElementById("password");
+const length =12;
+
+const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowercase="abcdefghijklmnopqrstuvwxyz"
+const number="0123456789"
+const symbol="@#$%^&*()_+~|}{[]></-="
+
+
+const allchart = uppercase + lowercase + number  + symbol;
+
+function creatpassword (){
+let password ="";
+password += uppercase[Math.floor(Math.random()*uppercase.length)];
+password += lowercase[Math.floor(Math.random()*lowercase.length)];
+password += number[Math.floor(Math.random()*number.length)];
+password += symbol[Math.floor(Math.random()*symbol.length)];
+while(length>password.length){
+    password += allchart[Math.floor(Math.random()*allchart.length)];
+}
+
+passwordbox.value =password
+}
+
+function copypass(){
+    passwordbox.select();
+    document.execCommand("copy");
+}
+
+
+
+
+
+
+
